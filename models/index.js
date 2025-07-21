9fe45103228cb00d5ae56f61fd0abb869e578641
tomasInit-01 <app@initsoluciones.com.ar>
2025-07-21 18:43:00 -0300

    d

--- models/index.js
+++ models/index.js
@@ -12,8 +12,8 @@ const db = {};
 let sequelize;
 const config = {
   database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST || '127.0.0.1',
   dialect: process.env.DB_DIALECT || 'mysql',
   port: process.env.DB_PORT || 3306
